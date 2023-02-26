c7a341e29ef374f6c85335d557805c923217e207
Taras Pustovyi <taraselnik@gmail.com>
2023-02-26 22:57:20 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ListValues } from "./components/ListValues";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/FS_challenge_front">
       <div className="container">
         <Routes>
           <Route path="/" element={<ListValues />} />
