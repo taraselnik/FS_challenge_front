b0b953ba186beb202d7d2e8f72a1e64267f979c4
Taras Pustovyi <taraselnik@gmail.com>
2023-02-26 23:31:52 +0200

    Add hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ListValues } from "./components/ListValues";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="container">
         <Routes>
           <Route path="/" element={<ListValues />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
